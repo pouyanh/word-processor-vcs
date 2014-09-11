5397d93f8a3245adf283f56a66adb7b618461e37
Pouyan Heyratpour <P.Heyratpour@Gmail.Com>
2014-09-12 01:43:24 +0430

    Yesss!
    Signed-off-by: Pouyan Heyratpour <P.Heyratpour@Gmail.Com>

--- src/wpvcs/view/welcome.cxx
+++ src/wpvcs/view/welcome.cxx
@@ -12,6 +12,7 @@ namespace Wpvcs {
 	Welcome::Welcome(const ::wxString& title, const ::wxPoint& pos, const ::wxSize& size)
 	: wxFrame(NULL, wxID_ANY, title, pos, size)
 	{
+	    CreateStatusBar();
 	    SetStatusText("Word Processor Version Control System");
 	}
     }
